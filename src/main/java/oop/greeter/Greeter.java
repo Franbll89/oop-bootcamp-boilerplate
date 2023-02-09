26edb7f612169aecd67235964c2c34f0c99aebfa
Claudia Lertora <claudia.lertora@thoughtworks.com>
2023-02-09 15:43:32 +0100

    Start morning greeting

--- src/main/java/oop/greeter/Greeter.java
+++ src/main/java/oop/greeter/Greeter.java
@@ -1,7 +1,23 @@
 package oop.greeter;
 
 public class Greeter {
+
+    private String time = "";
     public String greet(String name) {
-        return String.format("Hello %s", name);
+        String correctedName = name.trim();
+        String result = capitalizeName(correctedName);
+        String prefixGreeting = "Hello";
+        if (this.time.equals("06:00")) {
+            prefixGreeting = "Good morning";
+        }
+        return String.format("%s %s", prefixGreeting, result);
+    }
+
+    private static String capitalizeName(String name) {
+        return name.substring(0, 1).toUpperCase() + name.substring(1);
+    }
+
+    public void setTime(String time) {
+        this.time = time;
     }
 }
--- src/test/java/oop/greeter/GreeterTest.java
+++ src/test/java/oop/greeter/GreeterTest.java
@@ -1,27 +1,60 @@
 package oop.greeter;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreeterTest {
 
-    @Test
-    public void itShouldGreetClaudia() {
-        Greeter greeter = new Greeter();
+    private Greeter greeter;
 
+    @BeforeEach
+    void setup() {
+        greeter = new Greeter();
+    }
+    @Test
+    void itShouldGreetClaudia() {
         String actual = greeter.greet("Claudia");
 
         assertEquals("Hello Claudia", actual);
     }
 
     @Test
-    public void itShouldGreetFrancesco() {
-        Greeter greeter = new Greeter();
-
+    void itShouldGreetFrancesco() {
         String actual = greeter.greet("Francesco");
 
         assertEquals("Hello Francesco", actual);
     }
+
+    @Test
+    void itShouldTrimTheInput() {
+        String actual = greeter.greet(" Antonio ");
+
+        assertEquals("Hello Antonio", actual);
+    }
+
+    @Test
+    void itShouldCapitalizeTheFirstLetterOfTheName() {
+        String actual = greeter.greet("belen");
+
+        assertEquals("Hello Belen", actual);
+    }
+
+    @Test
+    public void itShouldGreetTheMorningAt6() {
+        greeter.setTime("06:00");
+        String actual = greeter.greet("Melissa");
+        assertEquals("Good morning Melissa", actual);
+    }
+
+    @Test
+    public void itShouldGreetTheMorningAt7() {
+        greeter.setTime("07:00");
+        String actual = greeter.greet("Melissa");
+        assertEquals("Good morning Melissa", actual);
+    }
+    
+    
 
 }
